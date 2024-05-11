2d7411e860825565834071bf6e23acda58c03b69
Gonzalo Jousse <95258624+GonzaTJ@users.noreply.github.com>
2024-05-10 21:43:12 -0300

    Add files via upload

--- generador_qr.py
+++ generador_qr.py
@@ -1,33 +1,51 @@
+import io
 import streamlit as st
 import qrcode
+import base64
 
-def generador_qr(url, filename):
+def generate_qr_code(input_text, qr_color):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(url)
+    qr.add_data(input_text)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(filename)
+    qr_img = qr.make_image(fill_color=qr_color, back_color="white")
+    
+    # Convertir la imagen a bytes
+    img_byte_arr = io.BytesIO()
+    qr_img.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    
+    return img_byte_arr
 
-st.title("Generador de QR")
+def main():
+    st.title("Generador de Códigos QR")
 
-# Campo de texto para ingresar la URL
-url = st.text_input("Ingrese la URL:")
+    # Input de texto o URL
+    input_text = st.text_input("Ingrese el texto o URL para generar el código QR:")
 
-# Botón para generar y descargar el QR
-if st.button("Generar y Descargar QR"):
-    if url:
-        filename = st.text_input("Nombre del archivo (sin extensión):", value="qr_code")
-        if filename:
-            filename += ".png"
-            generador_qr(url, filename)
-            st.success("QR generado y descargado correctamente como '{}'.".format(filename))
+    # Selector de color para el código QR
+    qr_color = st.color_picker("Seleccione el color para el código QR:", "#000000") # Color negro por defecto
+
+    if st.button("Generar QR"):
+        if input_text:
+            qr_img = generate_qr_code(input_text, qr_color)
+            st.image(qr_img, caption="Código QR generado", use_column_width=True)
+            st.markdown(get_image_download_link(qr_img), unsafe_allow_html=True)
         else:
-            st.error("Ingrese un nombre de archivo válido.")
-    else:
-        st.error("Ingrese una URL.")
+            st.warning("Por favor, ingrese un texto o URL válido.")
+
+def get_image_download_link(img_bytes):
+    """
+    Genera un enlace de descarga para la imagen.
+    """
+    encoded_img = base64.b64encode(img_bytes).decode()
+    href = f'<a href="data:file/png;base64,{encoded_img}" download="codigo_qr.png">Descargar Código QR</a>'
+    return href
+
+if __name__ == "__main__":
+    main()
